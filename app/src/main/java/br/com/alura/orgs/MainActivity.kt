b95cc0e6c206e77053f0a653476a33edcde6e86d
maria <mariazinha123@gmail.com>
2023-08-06 21:24:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/alura/orgs/MainActivity.kt
@@ -0,0 +1,15 @@
+package br.com.alura.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        setContentView(R.layout.activity_main)
+    }
+}
